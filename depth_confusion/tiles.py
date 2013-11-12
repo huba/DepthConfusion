60b1ed263a7eaa801761449c671b1118a79adc49
Huba Nagy <12huba@gmail.com>
2013-11-13 11:13:52 +1300

    Started implementation of flat tilemap world.

---
+++ depth_confusion/tiles.py
@@ -0,0 +1,15 @@
+"""
+Module with a class to represent a flat isometric tile grid
+Author: Huba Nagy
+"""
+import pygame
+
+
+
+class TiledWorld:
+	def __init__(self):
+		pass
+	
+	
+	def translate(self, dx, dy):
+		pass
